c0ce5c4edbc0cb884ea33c0855a3f1d3be634ffb
lpta2302 <lpta2302@users.noreply.github.com>
2024-02-06 06:02:33 +0000

    deploy: 09a5ab8cef4541eb3e6c9e436f656e130122227e

---
+++ assets/icons/index.js
@@ -0,0 +1,35 @@
+export { default as faviconRegular } from "./favicon-regular.png";
+export { default as favicon } from "./favicon.png";
+export { default as loader } from "./loader.svg";
+export { default as logOut } from "./logout.png";
+export { default as profilePlaceholder } from "./profile-user.png";
+
+export { default as darkIcon } from "./moon.png";
+export { default as lightIcon } from "./sun.png";
+export { default as search } from "./searchBtn.png";
+export { default as close } from "./close.png";
+
+export { default as success } from "./success.png";
+export { default as error } from "./error.png";
+export { default as warning } from "./warning.png";
+export { default as info } from "./information.png";
+
+export { default as returnIcon } from "./return.png";
+export { default as menu } from "./menu.png";
+export { default as home } from "./home.svg";
+export { default as explore } from "./explore.svg";
+export { default as people } from "./friend.png";
+export { default as addPost } from "./create-post.png";
+export { default as searchNav } from "./search.png";
+
+export { default as likeIcon } from "./like.png";
+export { default as likedIcon } from "./liked.png";
+
+export { default as deleteSaveIcon } from "./delete-save.png";
+export { default as saveIcon } from "./save.svg";
+export { default as savedIcon } from "./saved.svg";
+
+export { default as filePlaceholder } from "./file-upload.svg";
+export { default as edit } from "./edit.png";
+export { default as deleteIcon } from "./delete.svg";
+export { default as locationPin } from "./location-pin.png";
---
+++ assets/images/index.js
@@ -0,0 +1,13 @@
+export { default as webNameWhite } from './web-name-white.png';
+export { default as webName } from './web-name.png';
+
+export { default as sideImg } from './side-img.jpg';
+export { default as darkLogo } from './dark-logo.png';
+export { default as logo } from './logo.png';
+
+export { default as corgi } from './corgi.jpeg';
+
+export { default as worry } from './capoo-worrying.png';
+export { default as sad } from './capoo-sad.gif';
+export { default as surfing } from './surfingweb-capoo.gif';
+export { default as fun } from './nothing-mascot.png';
